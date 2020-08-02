407acd609130732fac7ce6b31101431f131c68b1
Anthony Hoss <BigT1305@users.noreply.github.com>
2020-08-02 13:20:11 -0500

    Changed README.md file

--- src/keys.js
+++ src/keys.js
@@ -1,4 +1,2 @@
-module.exports = {
-	API_KEY: 'bfffb1a631e1ccf272718a32d888e5ad',
-	BASE_URL: 'https://api.openweathermap.org/data/2.5/',
-};
+export const API_KEY = 'bfffb1a631e1ccf272718a32d888e5ad';
+export const BASE_URL = 'https://api.openweathermap.org/data/2.5/';
